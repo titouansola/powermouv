d2272a95261945e9186f18beee32055833305a8f
Titouan Sola <titouansola0@gmail.com>
2025-09-16 08:39:00 +0200

    Removed reference-pages

--- app/data/schemes/index.ts
+++ app/data/schemes/index.ts
@@ -2,7 +2,6 @@ import methods from './methods';
 import people from './people';
 import prices from './prices';
 import qna from './qna';
-import referencePages from './reference-pages';
 import solutions from './solutions';
 
-export const types = [methods, solutions, prices, qna, referencePages, people];
+export const types = [methods, solutions, prices, qna, people];
