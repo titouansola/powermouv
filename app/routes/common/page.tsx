0565717573cd441614ce671769eb3ac24108c61a
Titouan Sola <titouansola0@gmail.com>
2025-10-23 09:23:52 +0200

    V1.1.2

--- app/routes/common/page.tsx
+++ app/routes/common/page.tsx
@@ -1,8 +1,8 @@
 import { PortableText } from '@portabletext/react';
 import { ArticleList } from '~/common/components/ArticleList';
-import { urlFor } from '~/common/utils/image';
 import type { LightArticleModel } from '~/data/models/article-model';
 import type { PageModel } from '~/data/models/page-model';
+import type {PropsWithChildren} from "react";
 
 export function Page({
     page,
@@ -18,14 +18,35 @@ export function Page({
             >
                 <h1
                     className={
-                        'text-5xl font-bold mb-10 max-md:text-center'
+                        'text-5xl font-bold mb-10 text-center'
                     }
                 >
                     {page.title}
                 </h1>
-                <PortableText value={page.content} />
+                <PortableText
+                    value={page.content}
+                    components={{
+                        block: {
+                            h2: H2,
+                            h3: H3,
+                            h4: H4
+                        }
+                    }}
+                />
             </section>
             <ArticleList articles={articles} />
         </>
     );
+}
+
+function H2({ children }: PropsWithChildren) {
+    return <h2 className={'text-3xl'}>{children}</h2>
+}
+
+function H3({ children }: PropsWithChildren) {
+    return <h3 className={'text-2xl'}>{children}</h3>
+}
+
+function H4({ children }: PropsWithChildren) {
+    return <h4 className={'text-xl'}>{children}</h4>
 }
